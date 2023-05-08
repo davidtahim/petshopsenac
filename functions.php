23c8ba512d45fddc65808f7381c8238692f5c0a4
David Tahim <davidtahim@gmail.com>
2023-05-08 11:03:21 -0300

    widgets

--- functions.php
+++ functions.php
@@ -21,4 +21,20 @@
 		);
 	}
 	add_action ('init', 'register_theme_menus'); 
+	//começara criar os widgets
+	function create_widget ($name, $id, $description) {
+		register_sidebar( array(
+			'name' => __($name),
+			'id'=> $id,
+			'description' => __( $description ),
+			'before_widget' => '<div class="widget">',
+			'after_widget' => '</div>',
+			'before_title' => '<h3>',
+			'after_title' => '</h3>'
+		) );
+	}
+	create_widget('Front Page Left', 'front-left', 'exibe na lateral esquerda da página inicial');
+	create_widget('Front Page Center', 'front-center', 'exibe no centro esquerda da página inicial');
+	create_widget('Front Page Right', 'front-right', 'exibe na lateral direita da página inicial');
+
 ?>
